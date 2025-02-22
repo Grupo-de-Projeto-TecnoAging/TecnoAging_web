cd7025f78ce21d5417a8bee55ec7ab7151ef381a
Luccachl <lucca.hmcps@gmail.com>
2025-02-22 19:31:10 -0300

    feat: auth.middleware

--- backend/src/autenticacao/autenticacao.service.ts
+++ backend/src/autenticacao/autenticacao.service.ts
@@ -1,26 +1,32 @@
 import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import * as bcrypt from 'bcrypt';
+import { Pessoa } from 'src/pessoas/entities/pessoa.entity';
 
 @Injectable()
 export class AutenticacaoService {
   private readonly rodadasSalt = 10;
 
-  constructor(@InjectModel(Pessoas) private pessoasRepository: typeof Pessoas){}
+  constructor(
+    @InjectModel(Pessoa)
+    private pessoasRepository: typeof Pessoa){}
+
+
 
   async criptografarSenha(senha: string): Promise<string>{
-  const senhaHash = await bcrypt.hash(senha, this.rodadasSalt); 
-  return senhaHash;
+    const salt = await bcrypt.genSalt(this.rodadasSalt);
+    return bcrypt.hash(senha, salt); 
+   
 }
 
-  async login(email: string, senha:string): Promise<Pessoas>{
+  async login(email: string, senha:string): Promise<Pessoa>{
     const usuario = await this.pessoasRepository.findOne({where: {email} });
 
     if(!usuario) {
       throw new NotFoundException('Usuário não encontrado');
     }
 
-    const senhaCorreta = await bcrypt.compare(senha, usuario.senhaHash);
+    const senhaCorreta = await bcrypt.compare(senha, usuario.senha);
 
     if(!senhaCorreta){
       throw new UnauthorizedException('Senha incorreta');
