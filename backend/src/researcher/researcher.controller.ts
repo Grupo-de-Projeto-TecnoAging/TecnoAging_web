cd0429ff18162de9f294737e639868d74ee39f61
leopampa-ufpr <leonardodavi@ufpr.br>
2025-05-02 17:39:07 -0300

    fix/ researcher.controller fixed

--- backend/src/researcher/researcher.controller.ts
+++ backend/src/researcher/researcher.controller.ts
@@ -24,11 +24,11 @@ export class ResearcherController {
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateresearcherDto: UpdateResearcherDto) {
-    return this.researcherService.updateById(+id, updateresearcherDto);
+    return this.researcherService.update(+id, updateresearcherDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.researcherService.removeById(+id);
+    return this.researcherService.remove(+id);
   }
 }
